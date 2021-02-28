0d9d5aa7d6a9d6c024524e9df087f60958f7d4a8
BatisteBlactot20031998 <batiste.blactot.boulot@gmail.com>
2021-02-28 20:12:14 +0100

    add commentary and clean code

--- js/Pierre-Feuille-Ciseaux.js
+++ js/Pierre-Feuille-Ciseaux.js
@@ -1,38 +1,52 @@
-alert("Bonjour et bienvenue aventurier");
+        //Create a window alert for user
+
+    alert("Bonjour et bienvenue aventurier");
+
+        //Create two var: one var for user and one var for userchoice
+
 var userPlayer = prompt("Veuillez entrer votre nom pour commencer à jouer!");
 var userPlayerChoice = prompt("Merci " + userPlayer + "!" + "\n" + "Pouvez-faire un choix maintenant entre ciseaux, feuille et pierre");
-alert("Vous avez choisis " + userPlayerChoice);
+
+    alert("Vous avez choisis " + userPlayerChoice);
+
+        //Create a var for choices games with feuille, pierre and ciseaux
 
 var choice = [
     "pierre",
     "feuille",
     "ciseaux"
 ];
+        //Create a computer choice with a random number at Mathfloor - Mathrandom
 
 var computerPlayerChoice = Math.floor(Math.random() * choice.length);
 computerPlayerChoice = choice[computerPlayerChoice];
+
     alert("L'ordinateur a choisi " + computerPlayerChoice);
 
-    if(userPlayerChoice === "pierre" && computerPlayerChoice === "ciseaux"){
-        alert("You won!");
-    }
-    else if(userPlayerChoice === "pierre" && computerPlayerChoice === "pierre") {
-        alert("Draw!");
-    }
-    else if(userPlayerChoice === "feuille" && computerPlayerChoice === "pierre") {
-        alert("You won");
-    }
-    else if(userPlayerChoice === "ciseaux" && computerPlayerChoice === "feuille") {
-        alert("You won");
-    }
-    else if(userPlayerChoice === "feuille" && computerPlayerChoice === "feuille") {
-        alert("Draw");
-    }
-    else if(userPlayerChoice === "ciseaux" && computerPlayerChoice === "ciseaux") {
-        alert("Draw");
-    }
-    else {
-        alert("You lose!");
-    }
+        //Create conditions for differences at choices pierre feuille ciseaus for win, lose or draw
+
+            if(userPlayerChoice === "pierre" && computerPlayerChoice === "ciseaux"){
+    alert("You won!");
+            }
+            else if(userPlayerChoice === "pierre" && computerPlayerChoice === "pierre") {
+    alert("Draw!");
+            }
+            else if(userPlayerChoice === "feuille" && computerPlayerChoice === "pierre") {
+    alert("You won");
+            }
+            else if(userPlayerChoice === "ciseaux" && computerPlayerChoice === "feuille") {
+    alert("You won");
+            }
+            else if(userPlayerChoice === "feuille" && computerPlayerChoice === "feuille") {
+    alert("Draw");
+            }
+            else if(userPlayerChoice === "ciseaux" && computerPlayerChoice === "ciseaux") {
+    alert("Draw");
+            }
+            else {
+    alert("You lose!");
+            }
+
+//Create a window alert for say the game is finish
 
     alert("End of the game");
